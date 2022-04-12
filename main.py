8db8d4b02e83744e427fe71d64cab3fd26e89f61
ender <82971212+whcender@users.noreply.github.com>
2022-04-13 00:04:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
 import sqlite3
 
-from colorama import Cursor
-
 connect = sqlite3.connect("studens.db")
 
 cursor = connect.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS students(ogrencino INT, isim TEXT, soyisim TEXT, anneadı TEXT, babaadı TEXT, dogumtarihi INT, cinsiyet TEXT, adres TEXT, telefon INT, email TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS students(ogrencino INT, isim TEXT, anneadı TEXT, babaadı TEXT, dogumtarihi INT, cinsiyet TEXT, adres TEXT, telefon INT, email TEXT)")
 
-                                    # VERİ EKLEME #
+                                    # VERİ EKLEME # 
 
-# cursor.execute("INSERT INTO students VALUES(1, 'Adem', 'Mutlu', 'Gül', 'Recep', 2002, 'Erkek','İstanbul', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(2, 'Asu', 'Tekin', 'Ayşe', 'Şaban', 2001, 'Kadın','İstanbul', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(3, 'Pelin', 'Aslan', 'Ahsen', 'Kemal', 2001, 'Kadın','İstanbul', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(4, 'Serhat', 'Teke', 'Gülnur', 'Ramazan', 2001, 'Erkek','Bursa', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(5, 'Osman', 'Kartal', 'Fatma', 'Kamil', 2001, 'Erkek','Erzurum', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(6, 'Serhat', 'Karaoğlu', 'Sude', 'Dursun', 2001, 'Erkek','Ağrı', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(7, 'Büşra', 'Pekmez', 'Rana', 'Adem', 2001, 'Kadın','Kars', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(8, 'Eylül', 'Türkoğlu', 'Beyza', 'Tekin', 2002, 'Kadın','Antalya', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(9, 'Bergen', 'Apa', 'Betül', 'Fatih', 2002, 'Kadın','Antalya', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(10, 'Ender', 'Tekin', 'Sema', 'Dursun', 2002, 'Erkek','Hatay', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(11, 'Alp', 'Aras', 'Gül', 'Hamit', 2002, 'Erkek','Adana', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(12, 'İsa', 'Demir', 'Pelin', 'Efe', 2002, 'Erkek','Edirne', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(13, 'Talha', 'Demirtaş', 'Merve', 'Efecan', 2002, 'Erkek','Kastamonu', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(14, 'Ege', 'Taş', 'Ahsen', 'Buğra', 2002, 'Erkek','Malatya', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(15, 'Adnan', 'Kaplan', 'Ahu', 'Bahadır', 2001, 'Erkek','İzmir', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(16, 'Bekir', 'Buğra', 'Arzu', 'Caner', 2002, 'Erkek','İzmit', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(17, 'Fevzi', 'Tekin', 'Meryem', 'Haydar', 2002, 'Erkek','İzmit', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(18, 'Asel', 'Aslan', 'Berna', 'Polat', 2001, 'Kadın','İstanbul', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(19, 'Ela', 'Sever', 'Fulya', 'Demirkan', 2001, 'Kadın','Gaziantep', '05290000000', 'Email')")
-# cursor.execute("INSERT INTO students VALUES(20, 'Hale', 'Polat', 'Kevser', 'Fadıl', 2002, 'Kadın','Ankara', '05290000000', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(549398123, 'adem mutlu', 'Gül', 'Recep', 01.02.2002, 'Erkek','İstanbul', '05290000001', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(172312839, 'asu tekin', 'Ayşe', 'Şaban', 14.05.2001, 'Kadın','İstanbul', '05290000002', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(629123989, 'pelin aslan', 'Ahsen', 'Kemal', 26.06.2001, 'Kadın','İstanbul', '05290000003', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(716239012, 'ender tekin', 'Gülnur', 'Ramazan', 01.01.2002, 'Erkek','Bursa', '05290000004', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(887431912, 'osman kartal', 'Fatma', 'Kamil', 04.02.2002, 'Erkek','Erzurum', '05290000005', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(634417361, 'serhat karaoğlu', 'Sude', 'Dursun', 19.08.2001, 'Erkek','Ağrı', '05290000006', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(837120832, 'büşra pekmez', 'Rana', 'Adem', 26.09.2001, 'Kadın','Kars', '05290000007', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(939813823, 'eylül türkoğlu', 'Beyza', 'Tekin', 19.12.2002, 'Kadın','Antalya', '05290000008', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(123879173, 'bergen apa', 'Betül', 'Fatih', 25.11.2002, 'Kadın','Antalya', '05290000009', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(834719283, 'ender tekin', 'Sema', 'Dursun', 01.02.2002, 'Erkek','Hatay', '05290000010', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(973927123, 'alp aras', 'Gül', 'Hamit', 04.04.2002, 'Erkek','Adana', '05290000011', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(457638212, 'isa demir', 'Pelin', 'Efe', 11.11.2002, 'Erkek','Edirne', '05290000012', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(268612361, 'talha demirtaş', 'Merve', 'Efecan', 26.07.2002, 'Erkek','Kastamonu', '05290000013', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(762361937, 'ege taş', 'Ahsen', 'Buğra', 03.09.2002, 'Erkek','Malatya', '05290000014', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(737240180, 'adnan kaplan', 'Ahu', 'Bahadır', 07.09.2001, 'Erkek','İzmir', '05290000015', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(734129379, 'bekir buğra', 'Arzu', 'Caner', 01.12.2002, 'Erkek','İzmit', '05290000016', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(548719272, 'fevzi tekin', 'Meryem', 'Haydar', 09.03.2002, 'Erkek','İzmit', '05290000017', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(621681263, 'asel aslan', 'Berna', 'Polat', 09.03.2001, 'Kadın','İstanbul', '05290000018', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(347612321, 'ela sever', 'Fulya', 'Demirkan', 05.10.2001, 'Kadın','Gaziantep', '05290000019', 'Email')")
+# cursor.execute("INSERT INTO students VALUES(873246812, 'hale polat', 'Kevser', 'Fadıl', 19.10.2002, 'Kadın','Ankara', '05290000020', 'Email')")
 
 
 
                                     # VERİ ÇEKME #
 
-chose = int(input("işlem seçin: \n1. Numaraya Göre Veri Al \n2. İsme Göre Veri Al \n3. Yaşa Göre Veri Al \n4. Kompleks Arama \n5. İptal \nİşlem Seçin:"))
+chose = int(input("işlem seçin: \n1. Numaraya Göre Veri Al \n2. İsme Göre Veri Al \n3. Yaşa Göre Veri Al \n4. Telefon Numarasına Göre Veri Al \n5. İptal \nİşlem Seçin:"))
 
 if chose == 1:
     number = int(input("Numarayı Girin (1-20): "))
@@ -45,33 +43,26 @@ if chose == 1:
         print(data)
 
 elif chose == 2:
-    name = input("İsmi girin: ")
+    name = input("İsmi Soyismi girin (küçük harf kullanın): ")
 
-    cursor.execute(f"SELECT * from students WHERE isim == '{name.capitalize()}'")
+    cursor.execute(f"SELECT * from students WHERE isim == '{name}'")
     for data in cursor.fetchall():
-        print(data)
+        print(f"( numara: {data[0]}, isim soyisim: {data[1]}, anne adı:{data[2]}, baba adı: {data[3]}, doğum tarihi: {data[4]}, cinsiyet: {data[5]}, adres: {data[6]}, telefon: {data[7]}, email: {data[8]} )")
 
 elif chose == 3:
-    age = int(input("Yaş Değerini Girin (sadece yıl): "))
+    age = int(input("Doğum Tarihini Girin: "))
 
     cursor.execute(f"SELECT * from students WHERE dogumtarihi == {age}")
     for data in cursor.fetchall():
-        print(data)
+         print(f"( numara: {data[0]}, isim soyisim: {data[1]}, anne adı:{data[2]}, baba adı: {data[3]}, doğum tarihi: {data[4]}, cinsiyet: {data[5]}, adres: {data[6]}, telefon: {data[7]}, email: {data[8]} )")
 
 elif chose == 4: 
-    print("****************************************************************************************************")
-    search1 = input("1. Arama Filtresini Seçin (isim, soyisim, anneadı, babaadı): ")
-    search1 = search1.lower()
-
-    search2 = input("2. Arama Filtresini Seçin (isim, soyisim, anneadı, babaadı): ")
-    search2 = search2.lower()
-
-    find1 = input("1. filtrenin aramasını girin: ")
-    find2 = input("2. filtrenin aramasını girin: ")
-
-    cursor.execute(f"SELECT * from students WHERE {search1} == '{find1.capitalize()}' AND {search2} == '{find2.capitalize()}'")
+    telephone = int(input("Telefon numarası girin: "))
+    
+    cursor.execute(f"SELECT * from students WHERE telefon == {telephone}")
     for data in cursor.fetchall():
-        print(data)
+         print(f"( numara: {data[0]}, isim soyisim: {data[1]}, anne adı:{data[2]}, baba adı: {data[3]}, doğum tarihi: {data[4]}, cinsiyet: {data[5]}, adres: {data[6]}, telefon: {data[7]}, email: {data[8]} )")
+
 
 elif(chose) == 5:
     print("İşlem İptal Edildi")
@@ -81,5 +72,4 @@ else:
 
 
 connect.commit()
-
 connect.close()
